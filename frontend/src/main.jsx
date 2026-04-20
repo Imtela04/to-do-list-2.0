94352ff760a893f71bf3f9431080181293c00554
Imtela04 <imtela@gmail.com>
2026-04-20 18:27:30 +0600

    changed browserouter to hashrouter

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./style.css";
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
-        <BrowserRouter basename="/to-do-list-2.0">
+        <HashRouter basename="/to-do-list-2.0">
             <App />
-        </BrowserRouter>
+        </HashRouter>
     </StrictMode>
 );
